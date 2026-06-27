8ca2a38b977e237030ec76e87a1496344bc8f0c9
Rubel erald <your-rubelerald.j@gmail.com>
2026-06-27 13:59:17 +0530

    restfull server understand get post pust delete and send response acordingly

--- server.cjs
+++ server.cjs
@@ -15,11 +15,37 @@ const { json } = require("stream/consumers");
 // })
 
 // understanding the header how thr response have to be
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "content-type": "application/json" }); //=> 200 means status code
+// //   res.writeHead(201, { "content-type": "text/plain" });
+//   res.write(JSON.stringify({message : "hello world"}));
+//   res.end();
+// });
+// server.listen(3000, "localhost", () => {
+//   console.log("server is running");
+// });
+
+// making this server as restfull server , the server has to understand the methods get,post,put,delete
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "application/json" }); //=> 200 means status code 
-//   res.writeHead(201, { "content-type": "text/plain" });
-  res.write(JSON.stringify({message : "hello world"}));
-  res.end();
+  // console.log(req.url);
+  const parseURL = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = parseURL.pathname;
+  const method = req.method;
+  if (pathname === "/") {
+    if (method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.write(JSON.stringify({ message: "hello root" }));
+      res.end();
+    }
+  } else if (pathname === "/post") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.write(JSON.stringify({ message: "hello post" }));
+    res.end();
+  } else if (pathname === "/comments") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.write(JSON.stringify({ message: "hello comments" }));
+    res.end();
+  }
 });
 server.listen(3000, "localhost", () => {
   console.log("server is running");
