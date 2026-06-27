d2e3b82ba3008ec0501678d2dad20b23f233a610
Rubel erald <your-rubelerald.j@gmail.com>
2026-06-27 11:26:04 +0530

    creating a http server

---
+++ server.cjs
@@ -0,0 +1,16 @@
+// creating a server using node.js
+// first we need to import the http module
+const http = require('http');
+
+// creating http server 
+const server = http.createServer((req , res) => {
+    res.write("hello")
+    res.end()
+})
+
+
+// run the http server using node filename 
+server.listen(3000 , "localhost" , () => {
+    console.log("hello");
+})
+
